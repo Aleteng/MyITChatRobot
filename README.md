c9d026d1d44f317f5f88da974c5dd5afc70fe621
tiana_win10 <18120126@bjtu.edu.cn>
2019-03-31 00:21:46 +0800

    增加了托管功能，修改了发给文件传输助手会报错的bug。

--- README.md
+++ README.md
@@ -4,13 +4,16 @@
 介绍：登录微信，将收发信息统一打包为字典（dict）形式，然后使用post请求进行信息交换。就是一个可编写脚本控制、无界面的网页版微信客户端。<br>
 ## itchat库如何安装
 首先确保电脑上已安装python<br>
-然后pip安装：pip install -i https://pypi.douban.com/simple><br>
+然后pip安装：pip install -i https://pypi.douban.com/simple <br>
 ## 准备实现的功能
 首先考虑基于个人的，之后有缘再更新有关群消息回复的<br>
 其实就是一个参数的事，在注册时（@itchat.msg_register(TEXT, isFriendChat=True, isGroupChat=True,isMpChat=True)）想要哪个参数直接赋值为True即可，分别表示：<br>
 isFriendChat -- 来自好友的信息<br>
 isGroupChat -- 来自群消息<br>
 isMpChat -- 来自公众号消息<br>
-## 目前进度：1/100
-1.实现中英自动翻译：<br>
+## 目前进度：2/100
+1. 实现中英自动翻译：<br>
 当接收到的消息的以+_+开头，则自动识别语言翻译。<br>
+2. 实现 ** 托管 ** 功能：<br>
+向【文件传输助手】发送“取消托管”，可取消自动回复功能，发送“托管”则恢复自动回复功能，发送“托管状态”显示是否在托管中。<br>
+3. 准备实现语音/视频识别和转文字格式功能。【有缘再更新。。。】<br>
--- myFirstItChatRobot.py
+++ myFirstItChatRobot.py
@@ -7,48 +7,68 @@ from itchat.content import *
 from languageLibrary import *
 from Translate import *
 
+AUTOSWITCH = True # 全局开关
+
 @itchat.msg_register([TEXT, MAP, CARD, NOTE, SHARING], isFriendChat=True)
 def text_reply(msg):
-    # print(msg)
-    # if msg['User']['NickName'] == '田a':
-    print('Message from: %s' % msg['User']['NickName'])  # 在终端上打印发送者昵称
-    username = msg['User']['NickName']
-    messages = msg['Text']  # 消息
-    # 发送者的昵称
-    # username = msg['ActualNickName']
-    # print('Who sent it: %s' % username)
-    # 欢迎
-    if re.search('你好', messages) or re.search('hello', messages) or re.search('hi', messages) or re.search('田阿', messages):
-        itchat.send(HELLO['hello'],msg['FromUserName'])
-    # 汉译英和英译汉，带有+_+前缀为汉译英，带有=_=前缀为英译汉
-    if re.search('翻译', messages):
-        itchat.send('只要在你要翻译的内容前加上+_+就可以翻译咯', msg['FromUserName'])
     
-    if messages.strip()[:3] == '+_+':
-        ori_msg,zh_msg = youdao_trans(messages[3:], 1)
-        itchat.send(zh_msg, msg['FromUserName'])
-    match = re.search(u'工作', messages) or re.search(u'加班', messages)
-    if match:
-        print('-+-+' * 5)
-        print('Message content:%s' % msg['Content'])
-        print('工作、加班 is: %s' % (match is not None))
-        randomIdx = random.randint(0, len(REPLY['工作']) - 1)
-        itchat.send('%s\n%s' % (username, REPLY['工作'][randomIdx]), msg['FromUserName'])
-    match = re.search('学习', messages) or re.search('考试', messages)
-    if match:
-        print('-+-+' * 5)
-        print('Message content:%s' % msg['Content'])
-        print('学习、考试 is: %s' % (match is not None))
-        randomIdx = random.randint(0, len(REPLY['学习']) - 1)
-        itchat.send('%s\n%s' % (username, REPLY['学习'][randomIdx]), msg['FromUserName'])
-    # print('isAt is:%s' % msg['isAt'])
-    # if msg['isAt']:
-    #     randomIdx = random.randint(0, len(REPLY['default']) - 1)
-    #     itchat.send('%s\n%s' % (username, REPLY['default'][randomIdx]), msg['FromUserName'])
-    #     print('-+-+'*5)
+    switchOnAndOff(msg)
+    if not AUTOSWITCH:
+        # print('hello1')
+        return
+    # print(msg)
+    if msg['ToUserName'] != 'filehelper':
+        # print("hello2")
+        # if msg['User']['NickName'] == '田a':
+        print('Message from: %s' % msg['User']['NickName'])  # 在终端上打印发送者昵称
+        username = msg['User']['NickName']
+        messages = msg['Text']  # 消息
+        # 发送者的昵称
+        # username = msg['ActualNickName']
+        # print('Who sent it: %s' % username)
+        # 欢迎
+        if re.search('你好', messages) or re.search('hello', messages) or re.search('hi', messages) or re.search('田阿', messages):
+            itchat.send(HELLO['hello'],msg['FromUserName'])
+        # 汉译英和英译汉，带有+_+前缀为汉译英，带有=_=前缀为英译汉
+        if re.search('翻译', messages):
+            itchat.send('只要在你要翻译的内容前加上+_+就可以翻译咯', msg['FromUserName'])
 
-    # msg.user.send('%s: %s' % (msg.type, msg.text))
+        if messages.strip()[:3] == '+_+':
+            ori_msg,zh_msg = youdao_trans(messages[3:], 1)
+            itchat.send(zh_msg, msg['FromUserName'])
+        match = re.search(u'工作', messages) or re.search(u'加班', messages)
+        if match:
+            print('-+-+' * 5)
+            print('Message content:%s' % msg['Content'])
+            print('工作、加班 is: %s' % (match is not None))
+            randomIdx = random.randint(0, len(REPLY['工作']) - 1)
+            itchat.send('%s\n%s' % (username, REPLY['工作'][randomIdx]), msg['FromUserName'])
+        match = re.search('学习', messages) or re.search('考试', messages)
+        if match:
+            print('-+-+' * 5)
+            print('Message content:%s' % msg['Content'])
+            print('学习、考试 is: %s' % (match is not None))
+            randomIdx = random.randint(0, len(REPLY['学习']) - 1)
+            itchat.send('%s\n%s' % (username, REPLY['学习'][randomIdx]), msg['FromUserName'])
+        # print('isAt is:%s' % msg['isAt'])
+        # if msg['isAt']:
+        #     randomIdx = random.randint(0, len(REPLY['default']) - 1)
+        #     itchat.send('%s\n%s' % (username, REPLY['default'][randomIdx]), msg['FromUserName'])
+        #     print('-+-+'*5)
 
+        # msg.user.send('%s: %s' % (msg.type, msg.text))
+
+def switchOnAndOff(msg):
+  global AUTOSWITCH
+  if msg['ToUserName'] == 'filehelper':
+    if msg['Text'] == '启动托管':
+      AUTOSWITCH = True
+      itchat.send('托管中', toUserName='filehelper')
+    elif msg['Text'] == '取消托管':
+      AUTOSWITCH = False
+      itchat.send( 'OK', toUserName='filehelper')
+    elif msg['Text'] == '托管状态':
+      itchat.send(AUTOREPLYSTATUS[AUTOSWITCH], toUserName='filehelper')
 
 @itchat.msg_register([PICTURE, RECORDING, ATTACHMENT, VIDEO])
 def download_files(msg):
