edde5e62a0a282a60f48c22a97e14d7e5e11730a
tiana_win10 <18120126@bjtu.edu.cn>
2019-04-01 00:17:18 +0800

    修改了语音转文字模块，仍未完成

--- voiceRecognition.py
+++ voiceRecognition.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 
-import requests, json, uuid, base64
+import requests, json, uuid, base64, wave
 from secrets import *
 
 # 发送APIKEY和SECRETKEY，获取token
@@ -15,7 +15,28 @@ def get_token():
     return token
 
 # 通过get_token()获取的token，通过post，发送相关的语音识别信息到api，获取识别结果
-def recognize(sig, rate, token):
+def wave2text(sig, rate, token):
+    try:
+        wav_file = open(wav_file, 'rb')
+    except IOError:
+        print u'文件错误~'
+        return
+
+    # wav_file   = wave.open(wav_file)
+    # n_frames   = wav_file.getnframes()
+    # frame_rate = wav_file.getframerate()
+    # if n_frames == 1 or frame_rate not in (8000, 16000):
+    #     print u'不符合格式'
+    #     return
+    # audio   = wav_file.readframes(n_frames)
+    # seconds = n_frames/frame_rate + 1
+    # minute  = seconds/60 + 1
+    # for i in range(0, minute):
+    #     sub_audio    = audio[i*60*frame_rate:(i+1)*60*frame_rate]
+    #     base_data    = base64.b64encode(sub_audio)
+    #     access_token = ACCESS_TOKEN or get_token()
+
+
     urlVopApi = "http://vop.baidu.com/server_api"
     speech_length = len(sig)
     speech = base64.b64encode(sig).decode("utf-8")
@@ -44,4 +65,4 @@ if __name__ == "__main__":
     rate = 8000
     
     token = get_token()
-    recognize(signal, rate, token)
+    wave2text(signal, rate, token)
